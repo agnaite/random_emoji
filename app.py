d882b4737a508af42058fbdb47e3635a3338f954
Agnė Klimaitė <agnaite@me.com>
2016-07-20 19:11:14 -0700

    switched to sass

--- app.py
+++ app.py
@@ -2,9 +2,16 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template
+from flask_assets import Environment, Bundle
 from random import choice
 
 app = Flask(__name__)
+assets = Environment(app)
+
+assets.url = app.static_url_path
+scss = Bundle('sass.scss', filters='pyscss', output='all.css')
+assets.register('scss_all', scss)
+
 
 EMOJI = [u'🍄', u'🍕', u'🍪', u'🎨', u'🐌', u'🐦', u'🐶',
          u'👽', u'💀', u'💎', u'💜', u'💩', u'💰', u'🔮',
@@ -22,5 +29,4 @@ def home():
 if __name__ == '__main__':
     # server checks for changes
     # better error messages
-
     app.run(debug=True)
